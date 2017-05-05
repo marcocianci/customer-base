fd709ddb781759ed71ab415b850b4e3b44964e19
Marco Cianci <marco@cianci.com.br>
2017-05-05 02:42:39 -0300

    config de user id

--- backend/views/natural-person/_form.php
+++ backend/views/natural-person/_form.php
@@ -18,11 +18,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'rg')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    <?/*= $form->field($model, 'user_id')->textInput() */?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    <?/*= $form->field($model, 'created_at')->textInput() */?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?/*= $form->field($model, 'updated_at')->textInput() */?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
