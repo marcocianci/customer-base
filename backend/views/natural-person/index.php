38585c8cc209d6ed7048b2f3aa033f9e0270e056
Marco Cianci <marco@cianci.com.br>
2017-05-05 21:10:57 -0300

    Update index.php

--- backend/views/natural-person/index.php
+++ backend/views/natural-person/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Natural Person', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Criar Pessoa Fisica', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
